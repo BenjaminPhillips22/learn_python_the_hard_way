d1fe279b4e2960fcd26c08a7ac6e353d09061de1
ISELECT1\ben.phillips <ben.phillips@iselect.com.au>
2017-08-18 15:01:09 +1000

    first three exercises

---
+++ ex3.py
@@ -0,0 +1,20 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# the '%' is a modulus
+print(10 % 4)
