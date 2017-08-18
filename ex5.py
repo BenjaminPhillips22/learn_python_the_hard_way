916038521df0027ea2bb22842bebeab877a49927
ISELECT1\ben.phillips <ben.phillips@iselect.com.au>
2017-08-18 16:02:27 +1000

    some more exercises

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Benny J'
+my_age = 3
+my_height = 4
+my_weight = 5
+my_eyes = 'Brown'
+my_teeth = "Green"
+my_hair = "Blond?"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} meters tall.")
+print(f"He's {my_weight} kilograms in weight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"He's teeth are usually {my_teeth} because he only eats broccoli.")
+
+# is this a tricky line?
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
