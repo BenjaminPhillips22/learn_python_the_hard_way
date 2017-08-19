725e507fc960b921210107428dec4226da53bcad
Ben <benjamin.phillips@silverpond.com.au>
2017-08-19 20:25:05 +1000

    exercises 6-8

---
+++ ex7.py
@@ -0,0 +1,10 @@
+print("Mary had a little lamb.")
+print("it's fleece was white as {}.".format("snow"))
+
+print("." * 10)  # will print "." ten times
+
+end1 = "B"
+end2 = "e"
+end3 = "n"
+
+print(end1 + end2 + end3)
