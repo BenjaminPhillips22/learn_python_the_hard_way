0fd722573c72120a944d87a7bb2c1deae56ca474
Ben <benjamin.phillips@silverpond.com.au>
2017-09-09 20:26:09 +1000

    fun with functions

---
+++ ex18.py
@@ -0,0 +1,22 @@
+
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+
+def print_none():
+    print("I got nothin'.")
+
+
+print_two("Benny", "J")
+print_two_again("Benny", "JJ")
+print_one("First!")
+print_none()
