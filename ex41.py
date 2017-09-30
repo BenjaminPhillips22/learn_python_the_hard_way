fa71339ff098f0c1fff87d514b800748b9dc334f
Ben <benjamin.phillips@silverpond.com.au>
2017-09-30 16:59:32 +1000

    classes

---
+++ ex41.py
@@ -0,0 +1,47 @@
+# Class' and Objects
+
+
+class Person:
+    """
+    This is the doc for a persom,
+    Is it cool? We'll see.
+    """
+    personCount = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.personCount += 1
+
+    def displayCount(self):
+        print(f"Total Person count is; {Person.personCount}")
+
+    def displayPerson(self):
+        print("Name :", self.name, "Age: ", self.age)
+
+
+p1 = Person(name="Zara", age=99)
+p2 = Person(name="Manni", age=12)
+p1.displayPerson()
+p2.displayPerson()
+p1.displayCount()
+
+# We can also use 'hasattr', 'getattr', 'setattr' and 'delattr'
+print(hasattr(p1, 'name'))
+print(getattr(p1, 'name'))
+print(setattr(p1, 'name', 'Zara P'))
+print(delattr(p2, 'age'))
+try:
+    p2.displayPerson()
+except AttributeError:
+    print("There was an attribute error")
+
+# There are some built in class attributes
+
+print(Person.__doc__)
+print(Person.__name__)
+print(Person.__module__)
+print(Person.__bases__)
+print(Person.__dict__)
+
+print(p1.__class__)
