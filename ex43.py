88ea51f5b7e15522d644fd897e100b16c0a2a524
Ben <benjamin.phillips@silverpond.com.au>
2017-10-01 14:44:54 +1100

    composition

---
+++ ex43.py
@@ -0,0 +1,30 @@
+# Composition
+
+
+class Chocolate():
+
+    def __init__(self):
+        self.numberOfPiecesLeft = 10
+
+    def eatPiece(self):
+        self.numberOfPiecesLeft -= 1
+
+
+class Child():
+
+    def __init__(self, name):
+        self.choc = Chocolate()
+        self.name = name
+
+    def eatXPiecesOfChocolate(self, x):
+        for i in range(x):
+            self.choc.eatPiece()
+        print(f"\nYou now have {getattr(self.choc, 'numberOfPiecesLeft')}" +
+              " pieces of chocolate left.\n")
+
+
+boy = Child(name='Wilson')
+print(f"""
+You start of with
+{getattr(boy.choc, 'numberOfPiecesLeft')} pieces.""")
+boy.eatXPiecesOfChocolate(x=4)
